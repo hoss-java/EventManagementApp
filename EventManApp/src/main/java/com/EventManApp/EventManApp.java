c3e4c3064b65e4a175d9fd6b2f19ed319b19d2a7
Hossein <hossein@internal.nexttop.se>
2026-02-02 08:47:12 +0100

    [B001-C0009] Implement an UI class and it tests - try 1
    --merge
    
    Change-Id: I20a97b124a4c43d1195a025a7d48c4a94d5ab8c9

--- EventManApp/src/main/java/com/EventManApp/EventManApp.java
+++ EventManApp/src/main/java/com/EventManApp/EventManApp.java
@@ -14,7 +14,7 @@ import java.io.OutputStream;
 import java.util.Scanner;
 
 import com.EventManApp.lib.ConsoleCommand;
-
+//TEST
 /**
  * @file EventManApp.java
  * @brief Interactive console event managment application.
